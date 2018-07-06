ab093f3a9e0e74714b7150344c01c091b3d52687
William Raimundo Azevedo <willianray@gmail.com>
2018-07-06 18:19:53 -0300

    Arquivo de conexão e a base de dados.

---
+++ db/mongodb.js
@@ -0,0 +1,35 @@
+const { MongoClient } = require('mongodb');
+
+let connection = null;
+let db = null;
+
+const connect = () => new Promise((resolve, reject) => {
+    MongoClient.connect('mongodb://localhost/workshoptdc')
+        .then(conn => {
+            if (connection) {
+                return db;
+            }
+            connection = conn;
+            db = conn.db('workshoptdc');
+            return db;
+        })
+        .then(resolve)
+        .catch(err => {
+            console.log(err);
+            reject(err);
+        });
+});
+
+const disconnect = () => {
+    if(!connection){
+        return true;
+    }
+    connection.close();
+    connection = null;
+    return true;
+}
+
+module.exports = {
+    connect,
+    disconnect
+}
